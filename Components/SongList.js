102587082eea34874dc8c1d93fc8da0c08c6e3de
JimS-1987 <78493751+JimS-1987@users.noreply.github.com>
2021-08-23 11:59:48 +0200

    Add files via upload
    second commit

---
+++ Components/SongList.js
@@ -0,0 +1,16 @@
+import React from "react"
+import SongItem from "./SongItem"
+
+function SongList(props) {
+    const handleDeleteClick = props.handleClick
+    const songList = props.songs
+    const songsseperate = songList.map(element => <SongItem songs={element}
+        key={element.songId} handleClick={handleDeleteClick} />)
+    return (
+        <ul>
+            {songsseperate}
+        </ul>
+    )
+}
+
+export default SongList
---
+++ Components/SongOverview.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react"
+import SongForm from "./SongForm"
+import SongList from "./SongList"
+
+class SongOverview extends Component {
+
+  constructor() {
+    super()
+    this.state =
+    {
+      songsList: [
+        { songName: "Sitting on the dock of a bay", songArtist: "Otis Redding", songGenre: "Soul", songRating: "5", songId: 1 }
+      ],
+      itemId: 2
+    }
+    this.addSong = this.addSong.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
+  }
+
+  addSong = (song) => {
+    this.setState(prevState => { prevState.itemId++ })
+    console.log(this.state.itemId)
+    const addSong = { ...song, songId: this.state.itemId }
+    this.setState(prevState => {
+      const newSongList = [...prevState.songsList, addSong]
+      return { songsList: newSongList }
+    })
+  }
+  handleDelete(songItemId) {
+    let newSongList = this.state.songsList.filter(item => item.songId !== songItemId)
+    this.setState({ songsList: newSongList })
+  }
+
+  render() {
+    return (
+      <div>
+        <SongForm addSong={this.addSong} />
+        <SongList songs={this.state.songsList} handleClick={this.handleDelete} />
+      </div>
+    );
+  }
+}
+
+export default SongOverview;
